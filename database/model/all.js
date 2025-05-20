698c3b8666aa4c4b191074831dd485f4f57a697a
satriaabraarr <satria.abrarr@gmail.com>
2025-05-20 11:37:56 +0700

    Edit database/model Reports

--- database/model/all.js
+++ database/model/all.js
@@ -73,8 +73,10 @@ const logTransaksiSchema = new mongoose.Schema(
 
 const laporanSchema = new mongoose.Schema(
   {
-    amount    : { type: mongoose.Types.Decimal128, required: true },
-    created_by: { type: String, ref: 'User', required: true },
+    amount      : { type: Number, required: true },
+    type        : { type: String, enum: ['Income', 'Expense'], required: true },
+    description : { type: String, required: true },
+    created_by  : { type: String, ref: 'User', required: true },
   },
   { collection: "laporan", timestamps: true }
 );
