78fca04095f69b900ead4a6dacd36d9b36e963f8
abimafadricho <abima.fs401@gmail.com>
2025-05-21 12:35:10 +0700

    history

---
+++ app/dashboard/history/page.tsx
@@ -0,0 +1,12 @@
+// app/dashboard/reservations/page.tsx
+"use client"
+
+import { HistoryTable } from "@/components/history/HistoryTable"
+
+export default function ReservationsPage() {
+  return (
+    <div className="space-y-4 mt-4">
+      <HistoryTable />
+    </div>
+  )
+}
