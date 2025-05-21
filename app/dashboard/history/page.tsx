4f09ff4dd3e5193851a200e75e15e566d3e1b803
abimafadricho <abima.fs401@gmail.com>
2025-05-21 16:22:25 +0700

    resolve confict

--- app/dashboard/history/page.tsx
+++ app/dashboard/history/page.tsx
@@ -10,15 +10,3 @@ export default function ReservationsPage() {
     </div>
   )
 }
-// app/dashboard/reservations/page.tsx
-"use client"
-
-import { HistoryTable } from "@/components/history/HistoryTable"
-
-export default function ReservationsPage() {
-  return (
-    <div className="space-y-4 mt-4">
-      <HistoryTable />
-    </div>
-  )
-}
