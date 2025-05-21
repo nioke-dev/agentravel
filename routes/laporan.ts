cb85c4b0156bdd05592a97b42116e353448e2f19
satriaabraarr <satria.abrarr@gmail.com>
2025-05-21 12:37:25 +0700

    Edit routes/laporan.ts

--- routes/laporan.ts
+++ routes/laporan.ts
@@ -3,9 +3,15 @@ import { Laporan } from '@/database/model/all';
 
 const laporan = new Hono();
 
-laporan.get("/", async c => {
-  const data = await Laporan.find();
-  return c.json({ status: "berhasil", data });
-});
+laporan
+  .get("/", async c => {
+    const data = await Laporan.find();
+    return c.json({ status: "berhasil", data });
+  })
+  .get("/:id", async c => {
+    const { id } = c.req.param();
+    const data = await Laporan.findById(id);
+    return c.json({ status: "berhasil", data });
+  });
 
 export default laporan;
